36bbd6e4f49c128ccfe48c55c77d17e32829b8b5
Prince Prajapati <72757203+NotReallyPrince@users.noreply.github.com>
2024-04-13 18:50:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding="utf8") as readme:
 setuptools.setup(
     name="kellyapi",
     packages=setuptools.find_packages(),
-    version="dev.1",
+    version="0.0.1",
     license="MIT",
     description="A Project Made To Centralize Various APIs 📖 No Authorization Needed :)",
     long_description=long_description,
@@ -14,13 +14,13 @@ setuptools.setup(
     author="NotReallyPrince",
     author_email="princebots3011@gmail.com",
     url="https://github.com/NotReallyPrince/Prince-Api",
-    keywords=["Prince-Api", "python-pakage", "api", "Prince"],
+    keywords=["Kelly-API", "Kelly", "KellyAI", "KellyAPI"],
     install_requires=["dotmap", "aiohttp"],
     project_urls={
-        "Tracker": "https://github.com/NotReallyPrince/Prince-API/issues",
+        "Tracker": "https://github.com/NotReallyPrince/Kelly-API/issues",
         "Community": "https://t.me/PrincexUpdates",
-        "Source": "https://github.com/NotReallyPrince/Prince-API",
-        "Documentation": "https://docs.princexd.tech",
+        "Source": "https://github.com/NotReallyPrince/Kelly-API",
+        "Documentation": "https://docs.kellyai.pro",
     },
     classifiers=[
         "Development Status :: 5 - Production/Stable",
